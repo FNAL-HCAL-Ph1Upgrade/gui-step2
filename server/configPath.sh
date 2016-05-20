3b832c0dd674fdfc41d1fd77cedefea0af39abbb
Jordan Potarf <jordan_potarf@baylor.edu>
2016-05-20 19:38:45 +0000

    reworked server and client, still needs test

---
+++ server/configPath.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=//usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+export LD_LIBRARY_PATH=/home/pi/gnublin-api:/usr/lib/:/usr/share/boost-build/
+
