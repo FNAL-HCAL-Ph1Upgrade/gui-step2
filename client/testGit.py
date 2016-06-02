0afde4801975fc979e3b18ca23581a9afdb65668
Shaun Hogan <sphogan@crimson.ua.edu>
2016-06-02 11:23:20 -0500

    Made simple changes to test git functionality.

---
+++ client/testGit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#
+#inputFile.py
+#a test input file for the client
+################################################################################
+# imports - must have in all files using these commands
+################################################################################
+from client import SR, SW, CR, CW, SRs, CRs
+################################################################################
+
+################################################################################
+# test-specific functions - defined by tester
+################################################################################
+def SIMPLE_CHECK_WRITE(address, value):
+    SRs(address) #for reference
+    SWs(address, value)
+    if SR(address) == value:
+        return True
+    else:
+        return False
+
+def COMPLEX_CHECK_WRITE(address, register, value):
+    CR(address, register) #for reference
+    CW(address, register, value)
+    if CR(address, register) == value:
+        return True
+    else:
+        return False
+################################################################################
+
+
+################################################################################
+# Test - the test() function is executed by the client
+################################################################################
+
+def test():
+    print SRs(0x72)
+    SIMPLE_CHECK_WRITE(0x72, 0x01)
+    print SRs(0x72)
+    SIMPLE_CHECK_WRITE(0x72, 0x03)
+################################################################################
