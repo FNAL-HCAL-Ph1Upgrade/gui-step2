012f432972cb93d244030912a53aee921b734069
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-06 09:48:26 -0500

    hi

--- client/inputFile.py
+++ client/inputFile.py
@@ -34,8 +34,8 @@ def COMPLEX_CHECK_WRITE(address, register, value):
 ################################################################################
 
 def test():
-    print SR(0x72)
+    SR(0x72)
     # SIMPLE_CHECK_WRITE(0x72, 0x01)
-    print SR(0x72)
+    SR(0x72)
     # SIMPLE_CHECK_WRITE(0x72, 0x02)
 ################################################################################
