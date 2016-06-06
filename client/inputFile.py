ae9afe4f3ac1dd55ae9dd235ef79424eb6e127c2
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-06 10:20:22 -0500

    committed

--- client/inputFile.py
+++ client/inputFile.py
@@ -27,17 +27,32 @@ def COMPLEX_CHECK_WRITE(address, register, value):
     else:
         return False
 ################################################################################
-
-
-################################################################################
-# Test - the test() function is executed by the client
-################################################################################
-
-def test():
+# 0. Initial Test
+def initial_test():
     print SR(0x71) == None
     print SRs(0x71) == None
     # SW(0x72,0x01)
     # SR(0x74)
     # SIMPLE_CHECK_WRITE(0x72, 0x01)
     # SIMPLE_CHECK_WRITE(0x72, 0x02)
+
+# 1. Determine address of Fanout (0x72 unless switches are flipped).
+def find_fan(start):
+    val = None
+    while val == None or start > 0xa0:
+        val = SR(start)
+        start += 1
+    return val
+
+# 2. Determine bits to set on Fanout to open i2c with ngCCM FPGA Bridge (0x74). Verify address of Fanout.
+
+# 3. Determine
+
+################################################################################
+# Test - the test() function is executed by the client
+################################################################################
+
+def test():
+
+
 ################################################################################
--- client/register_test_1.py
+++ client/register_test_1.py
@@ -2,3 +2,9 @@
 for bit in range(8):
     a = 2**bit
     print "Bit ", bit, " -> Hex ", hex(a),  " -> Dec ", a
+
+# 1. Determine address of Fanout (0x72 unless switches are flipped).
+
+# 2. Determine bits to set on Fanout to open i2c with ngCCM FPGA Bridge (0x74). Verify address of Fanout.
+
+# 3. Determine
