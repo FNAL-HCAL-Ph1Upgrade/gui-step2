03ce82d43f87346745b72a04008f3914a18886a7
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-08 11:22:10 -0500

    committed

--- client/igloo_test.py
+++ client/igloo_test.py
@@ -6,8 +6,8 @@ q = QIELib
 def igloo(rm,slot):
     q.openChannel(rm,slot)
     b.write(q.QIEi2c[slot],[0x11,0x03,0,0,0])
-    # b.write(0x09,[0x05])
-    # b.read(0x09,8)
+    b.write(0x09,[0x05])
+    b.read(0x09,8)
     return b.sendBatch()
 
 print igloo(0,0)
