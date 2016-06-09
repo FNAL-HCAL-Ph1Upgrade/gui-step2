03b35b967b07b8005165137b1a3583769d6399af
adrysmith <adryanna_smith@baylor.edu>
2016-06-09 14:30:41 -0500

    fucntions for reading from  the VTTX registers

--- client/vttxTest.py
+++ client/vttxTest.py
@@ -3,7 +3,7 @@ import QIELib
 b = webBus("pi5")
 q = QIELib
 
-def vttxOpen(rm,slot):
+def vttx1read(rm,slot):
     q.openChannel(rm,slot)
     #VTTX 1 is value "1" in I2C_SELECT table
     b.write(q.QIEi2c[slot],[0x11,0x01,0,0,0])
@@ -13,4 +13,15 @@ def vttxOpen(rm,slot):
 
     return data
 
-print "From vttxOpen: " + vttxOpen(0,0)
+def vttx2read(rm,slot):
+    q.openChannel(rm,slot)
+    #VTTX 1 is value "1" in I2C_SELECT table
+    b.write(q.QIEi2c[slot],[0x11,0x02,0,0,0])
+    b.write(0x7E,[0x00])
+    b.read(0x7E,7)
+    data = b.sendBatch()[2]
+
+    return data
+
+print "From vttx1read: " + vttx1read(0,0)
+print "From vttx2read: " + vttx2read(0,0)
