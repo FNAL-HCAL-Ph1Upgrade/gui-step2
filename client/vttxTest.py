efae931690f080bc9aa9c3a716a6e3156647cc0d
adrysmith <adryanna_smith@baylor.edu>
2016-06-09 14:51:32 -0500

    fixing index prob

--- client/vttxTest.py
+++ client/vttxTest.py
@@ -41,7 +41,7 @@ def vttx1write(rm,slot):
     #VTTX 1 is value "1" in I2C_SELECT table
     b.write(q.QIEi2c[slot],[0x11,0x01,0,0,0])
     b.write(0x7E,[0x00,0x87,0x99,0x19,0x88,0xff,0xff,0x04])
-    data = b.sendBatch()[2]
+    data = b.sendBatch()[1]
 
     return data
 
@@ -50,7 +50,7 @@ def vttx2write(rm,slot):
     #VTTX 1 is value "1" in I2C_SELECT table
     b.write(q.QIEi2c[slot],[0x11,0x02,0,0,0])
     b.write(0x7E,[0x00,0x87,0x99,0x19,0x88,0xff,0xff,0x04])
-    data = b.sendBatch()[2]
+    data = b.sendBatch()[1]
 
     return data
 
