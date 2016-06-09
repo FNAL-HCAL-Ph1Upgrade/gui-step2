fa0c1242dcd58268861e855469c1dbc7a0b179d3
adrysmith <adryanna_smith@baylor.edu>
2016-06-09 14:23:45 -0500

    committing...

--- client/vttxTest.py
+++ client/vttxTest.py
@@ -7,6 +7,10 @@ def vttxOpen(rm,slot):
     q.openChannel(rm,slot)
     #VTTX 1 is value "1" in I2C_SELECT table
     b.write(q.QIEi2c[slot],[0x11,0x01,0,0,0])
-    b.sendBatch()
+    b.write(0x7E,[0x00])
+    b.read(0x7E,7)
+    data = b.sendBatch()[2]
 
-vttxOpen(0,0)
+    return data
+
+print "From vttxOpen: " + vttxOpen(0,0)
