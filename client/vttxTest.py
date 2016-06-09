e39a76fa313e03dae8397ac07d3e2362d57666e6
adrysmith <adryanna_smith@baylor.edu>
2016-06-09 14:49:31 -0500

    Adding in write capabilities to vttx

--- client/vttxTest.py
+++ client/vttxTest.py
@@ -3,6 +3,10 @@ import QIELib
 b = webBus("pi5")
 q = QIELib
 
+########################################################
+# VTTX Read Functions
+########################################################
+
 def vttx1read(rm,slot):
     q.openChannel(rm,slot)
     #VTTX 1 is value "1" in I2C_SELECT table
@@ -25,3 +29,30 @@ def vttx2read(rm,slot):
 
 print "From vttx1read: " + vttx1read(0,0)
 print "From vttx2read: " + vttx2read(0,0)
+
+
+
+########################################################
+# VTTX Write Functions
+########################################################
+
+def vttx1write(rm,slot):
+    q.openChannel(rm,slot)
+    #VTTX 1 is value "1" in I2C_SELECT table
+    b.write(q.QIEi2c[slot],[0x11,0x01,0,0,0])
+    b.write(0x7E,[0x00,0x87,0x99,0x19,0x88,0xff,0xff,0x04])
+    data = b.sendBatch()[2]
+
+    return data
+
+def vttx2write(rm,slot):
+    q.openChannel(rm,slot)
+    #VTTX 1 is value "1" in I2C_SELECT table
+    b.write(q.QIEi2c[slot],[0x11,0x02,0,0,0])
+    b.write(0x7E,[0x00,0x87,0x99,0x19,0x88,0xff,0xff,0x04])
+    data = b.sendBatch()[2]
+
+    return data
+
+print "From vttx1write: " + vttx1write(0,0)
+print "From vttx2write: " + vttx2write(0,0)
