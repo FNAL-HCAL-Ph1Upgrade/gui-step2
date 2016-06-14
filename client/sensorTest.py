20ba1f771aa38af0e064d03579562339c9155d2a
caleb-james-smith <bosonbaas@gmail.com>
2016-06-14 10:46:18 -0500

    cleaning

--- client/sensorTest.py
+++ client/sensorTest.py
@@ -12,10 +12,6 @@ def sensorTemp(rm,slot):
     b.read(0x40,2)
 
     data = b.sendBatch()[2]
-    #Splitting the incoming data and reversing the order of bytes
-    #(the read function's inherent reversal of bytes isn't compatible with
-    #this particular sensor's desired output order)
-
     # Splitting the incoming data & concatenating strings of binary bytes
     data = format(int(data.split()[0]),'08b') + format(int(data.split()[1]),'08b')
     # zero-ing out the 2 status bits, converting to int for calculations
@@ -23,11 +19,9 @@ def sensorTemp(rm,slot):
     # Converting temp using equation
     temp = (-46.85) +175.72*(data)/(2**16)
     
-    print "                             Temp DATA = " + str(data)    
-
     return temp
 
-print "                                  " +  "%.2f" %(sensorTemp(0,2))
+print "%.2f" %(sensorTemp(0,2))
 
 
 #returns the relative humidity of the QIE card
@@ -45,8 +39,6 @@ def sensorHumid(rm,slot):
     # Converting humidity using equation
     humid = (-6.0) + 125.0 * (data)/(2**16)
 
-    print "                             Humid DATA = " + str(data)
-
     return humid
 
-print "                                 " + "%.2f" %(sensorHumid(0,2))
+print "%.2f" %(sensorHumid(0,2))
