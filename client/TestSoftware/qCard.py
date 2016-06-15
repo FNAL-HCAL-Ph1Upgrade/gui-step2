b4f5a0d3fc3107d7b499603f5368e48c6a4457f5
potarf <jordan_potarf@baylor.edu>
2016-06-15 09:49:37 -0500

    Functioning tests from entire teststand

--- client/TestSoftware/qCard.py
+++ client/TestSoftware/qCard.py
@@ -1,6 +1,5 @@
 #qCard class
 import tests
-from tests import testList
 
 cardAddresses = [0x19, 0x1A, 0x1B, 0x1C]
 
@@ -18,18 +17,17 @@ class qCard:
         self.address = getCardAddress(slot)
         self.barcode = barcode
         self.passed = []
-        for test in tests.testList:
-            self.passed.append(0)
     def __repr__(self):
         '''Object representation'''
         return "qCard()"
     def __str__(self):
         '''Return string representation of card information'''
-        s = "qCard: " + str(self.barcode) + '\n'
-        for i in xrange(len(tests.testList)):
-            s += "%s: %d \n" % (str(tests.testList[i]), self.passed[i])
+        s = "qCard: %s at slot %s\n" % (self.barcode, self.slot)
+        for i in xrange(len(self.passed)):
+            s += "Test%s: %s \n" % (i, str(self.passed[i]))
         return s
     def runAll(self):
         '''Run all tests'''
-        for i in xrange(len(tests.testList)):
-            self.passed[i] = tests.testList[i](self.address)
+        t = tests.testSuite("pi5", self.address)
+        for result in t.runTests():
+            self.passed.append(result)
