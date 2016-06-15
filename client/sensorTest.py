235f33167a795088df734899501d140113a7dbd3
adrysmith <adryanna_smith@baylor.edu>
2016-06-15 12:20:33 -0500

    trying to detect the 3rd byte of output, the checksum

--- client/sensorTest.py
+++ client/sensorTest.py
@@ -9,15 +9,19 @@ def sensorTemp(rm,slot):
     q.openChannel(rm,slot)
     b.write(q.QIEi2c[slot],[0x11,0x05,0,0,0])
     b.write(0x40,[0xf3])
-    b.read(0x40,2)
+    b.read(0x40,3)
 
-    data = b.sendBatch()[2]
+    data = (b.sendBatch()[2]).split()
+    checksum = data[2] # 3rd byte of data is the checksum
+    print "Checksum: " + format(int(checksum),'08b')
+
     #Splitting the incoming data and reversing the order of bytes
     #(the read function's inherent reversal of bytes isn't compatible with
     #this particular sensor's desired output order)
 
     # Splitting the incoming data & concatenating strings of binary bytes
     data = format(int(data.split()[0]),'08b') + format(int(data.split()[1]),'08b')
+    print "2 data bytes: " + data
     # zero-ing out the 2 status bits, converting to int for calculations
     data = int(data[0:14] + "00", 2)
     # Converting temp using equation
