859e048b20c5c0ad737ef5936d3619ecdf20ce5d
adrysmith <adryanna_smith@baylor.edu>
2016-06-17 15:40:35 -0500

    commit in testing

--- client/IglooLib.py
+++ client/IglooLib.py
@@ -37,6 +37,7 @@ def catBitsFromBytes(bitList, first = 0, length = 0):
     if (length == 0):
         for i in bitList:
             bitString = bitString + i
+            print "bitString: ", bitString
     elif (length > 0):
     	for i in bitList[first:(first+length)]:
     		bitString = bitString + i
