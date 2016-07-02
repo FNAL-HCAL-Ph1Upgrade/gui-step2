06bd45cb3974f25ac3e87256d4ec10e1992ad573
adrysmith <adryanna_smith@baylor.edu>
2016-07-01 19:43:48 -0500

    Making shunt scan understand 2-peak CI in uHTR.py; Added adc to fc conversion to Hardware.py

--- client/TestSoftware/scratch.py
+++ client/TestSoftware/scratch.py
@@ -1,7 +1,48 @@
-import TestSoftware.Hardware
+class ADCConverter:
+    # Bitmasks for 8-bit ADC input
 
-h = TestSoftware.Hardware
+    expMask = 192
+    manMask = 63
+    baseSensitivity = 3.1
+    # Base charges for each subrange
+    # Use array for which 0 ADC = 0 fC input charge
+    inputCharge = [
+        -1.6, 48.4, 172.4, 433.4, 606.4,
+        517, 915, 1910, 3990, 5380,
+        4780, 7960, 15900, 32600, 43700,
+        38900, 64300, 128000, 261000, 350000
+    ]
 
-myHardware = h.Hardware()
+    #Defines the size of the ADC mantissa subranges
+    adcBase = [0, 16, 36, 57, 64]
 
-print myHardware.cardAddresses
+    # A class to convert ADC to fC
+    fc = {}
+
+    def __init__(self):
+        # Loop over exponents, 0 - 3
+        for exp in xrange(0, 4):
+            # Loop over mantissas, 0 - 63
+            for man in xrange(0, 64):
+                subrange = -1
+                # Find which subrange the mantissa is in
+                for i in xrange(0, 4):
+                    if man >= self.adcBase[i] and man < self.adcBase[i + 1]:
+                        subrange = i
+
+                if subrange == -1:
+                    print "Something has gone horribly wrong!"
+
+                # Sensitivity = 3.1 * 8^exp * 2^subrange
+                sensitivity = self.baseSensitivity * 8.0**float(exp) * 2.0**subrange
+                # Add sensitivity * (location in subrange) to base charge
+                #fc[exp * 64 + man] = (inputCharge[exp * 5 + subrange] + ((man - adcBase[subrange])) * sensitivity) / gain;
+                self.fc[exp * 64 + man] = self.inputCharge[exp * 5 + subrange] + ((man - self.adcBase[subrange]) + .5) * sensitivity
+
+    def linearize(self, adc):
+        return self.fc[adc]
+
+
+myadc = ADCConverter()
+for i in xrange(255):
+    print "%d ADC: " %i, myadc.linearize(i)
