de70c84b891a0387e97d7c46a04b34ad82427322
hcalpro <hcalpro@cmsnghcal01.fnal.gov>
2018-06-22 16:38:55 -0500

    added terrifying script hacks

---
+++ client/TestSoftware/FixMe-Everything.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/hcalpro/hcalraw-scripts/processPluginsHBQIE
+./RunEverything.sh $1 
---
+++ client/TestSoftware/FixMe-RegisterTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Hack to run the register test from the gui
+cd /home/hcalpro/GITrepos/Common/
+python RunRegisterTest.py $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17}
