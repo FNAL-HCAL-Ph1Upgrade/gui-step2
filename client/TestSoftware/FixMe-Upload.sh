d10d4bacdbc35baab853fc80e43c4845dcfe84bf
hcalpro <hcalpro@cmsnghcal01.fnal.gov>
2018-06-27 12:48:40 -0500

    Added run # checks and updated scripts

--- client/TestSoftware/FixMe-Upload.sh
+++ client/TestSoftware/FixMe-Upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/hcalpro/hcalraw-scripts/processPluginsHBQIE
-./MoveFiles.sh $1
-ssh hep@cmshcal11 '/home/django/testing_database_hb/uploader/upload_step2.sh'
-echo Finished with upload
+./MoveFiles.sh $1 $2 $3 
+eval ssh hep@cmshcal11 '/home/django/testing_database_hb/uploader/upload_step2.sh' $4
+Finished with upload
 
