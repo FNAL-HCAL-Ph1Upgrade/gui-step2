6c6c01a068b4ffa3bc5ee7b35e8d4679618a7fc9
hcalpro <hcalpro@cmsnghcal01.fnal.gov>
2018-06-27 11:06:53 -0500

    added dependant script

---
+++ client/TestSoftware/FixMe-PluginOut.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+runNum=$1
+cd /home/hcalpro/hcalraw-scripts/processPluginsHBQIE
+./processPlugins -f /home/hcalpro/hcalraw/output/run$runNum-master.root
+echo Finished running plugin output
