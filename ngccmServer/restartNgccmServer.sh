26aae812e3bb90bbd793b8ee1b7ae2ae9d0ada30
hcalpro <hcalpro@cmsnghcal01.fnal.gov>
2018-07-01 09:05:51 -0500

    store pid from pgrap to use to kill ngccm server. search for pid based on port.

--- ngccmServer/restartNgccmServer.sh
+++ ngccmServer/restartNgccmServer.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
+# restartNgccmServer.sh
+# Caleb Smith --- July 1, 2018
+# use port to search for ngccm server process
+# kill and then start ngccm server if it is running
+# start ngccm server if it is not running
+
+V=true
+user=hcalpro
+startServer=/home/hcalpro/ngFEC/start_ngccm_hehbfermi_00.sh
+port=64000
 
 #check if ngccm server is running
 
-V=true
-
-if pgrep -f ngccm >/dev/null 2>&1 ; then
+#if pgrep -f ngccm >/dev/null 2>&1 ; then
+if pid=$(pgrep -f $port) ; then
     # kill and start ngccm server
     
     # kill ngccm server
     if $V ; then
-        echo "ngccm server is already running"
-        echo "using kill to send ngccm server into the grave"
+        echo "ngccm server is already running on port $port with pid $pid"
+        echo "using kill to send ngccm server on port $port with pid $pid to the grave"
     fi
+    #sudo -u $user kill $pid
     # start ngccm server
     if $V ; then
-        echo "starting ngccm server... because the server will always rise again"
+        echo "starting ngccm server on port "$port"... because the server will always rise again"
     fi
+    #sudo -u $user $starServer
 else
     # start ngccm server
     if $V ; then
-        echo "ngccm server is not running"
-        echo "starting ngccm server..."
+        echo "ngccm server on port $port is not running"
+        echo "starting ngccm server on port "$port"... I look forward to our collaboration"
     fi
-    #source /home/pi/NGCCMeFEC/server/configPath.sh
-    #nohup python /home/pi/NGCCMeFEC/server/ngccm server > /dev/null 2>&1 &
+    #sudo -u $user $starServer
+    # > /dev/null 2>&1 &
 fi
+
