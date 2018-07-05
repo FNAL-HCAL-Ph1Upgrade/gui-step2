babfcdf2771bf9af5889c971581828b81cfd277b
hcalpro <hcalpro@cmsnghcal01.fnal.gov>
2018-07-05 10:09:18 -0500

    Add kill and start server commands. Now seach for ngccm server process using tag because search with port also returns client processes. Fix spelling of startServer.

--- ngccmServer/restartNgccmServer.sh
+++ ngccmServer/restartNgccmServer.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 # restartNgccmServer.sh
 # Caleb Smith --- July 1, 2018
-# use port to search for ngccm server process
+# use tag to search for ngccm server process
 # kill and then start ngccm server if it is running
 # start ngccm server if it is not running
 
 V=true
-user=hcalpro
-startServer=/home/hcalpro/ngFEC/start_ngccm_hbfermi_00.sh
+user="hcalpro"
+startServer="/home/hcalpro/ngFEC/start_ngccm_hbfermi_00.sh"
+#startServer=/home/hcalpro/ngFEC/ngccm-0.0.12-63.el7/opt/ngccm/scripts/start_ngccm_hbfermi_00.sh
 port=64000
+tag="hbfermi_00"
+
+echo "start server script: $startServer"
+ls -l $startServer
 
 #check if ngccm server is running
+#kill the server if it is running
 
 #if pgrep -f ngccm >/dev/null 2>&1 ; then
-if pid=$(pgrep -f $port) ; then
-    # kill and start ngccm server
-    
+if pid=$(pgrep -f $tag) ; then
     # kill ngccm server
     if $V ; then
-        echo "ngccm server is already running on port $port with pid $pid"
-        echo "using kill to send ngccm server on port $port with pid $pid to the grave"
+        echo "ngccm server with tag $tag is already running with pid $pid"
+        echo "using kill to send ngccm server with tag $tag and pid $pid to the grave"
     fi
-    #sudo -u $user kill $pid
-    # start ngccm server
-    if $V ; then
-        echo "starting ngccm server on port "$port"... because the server will always rise again"
-    fi
-    #sudo -u $user $starServer
+    kill $pid
 else
-    # start ngccm server
-    if $V ; then
-        echo "ngccm server on port $port is not running"
-        echo "starting ngccm server on port "$port"... I look forward to our collaboration"
-    fi
-    #sudo -u $user $starServer
-    # > /dev/null 2>&1 &
+    echo "ngccm server with tag $tag is not running"
+fi
+
+# start ngccm server
+
+if $V ; then
+    echo "starting ngccm server with tag "$tag"... because the server will always rise again"
+fi
+# bash $startServer > /dev/null 2>&1 &
+$startServer
+return_value=$?
+if [ $return_value -eq 0 ]; then
+    echo "Success: started ngccm server"
+else
+    echo "ERROR: the start ngccm server script returned nonzero error code: $return_value"
 fi
 
